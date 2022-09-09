4c18aab0e148f28b2e5d00dfea10dd098f8b358b
JCarlosMtzZ <88814350+JCarlosMtzZ@users.noreply.github.com>
2022-09-09 16:23:45 -0500

    Delete Evidencia2-Presentación directory

--- Evidencia2-Presentación/Implementación-Agentes/run.py
+++
@@ -1,8 +0,0 @@
-# Juan Carlos Martínez Zacarías - A01612967
-# Sergio Alejandro Esparza González - A01625430
-# Joaquín Aguirre de la Fuente - A01177479
-# H. Daniel Martínez Rodríguez - A01177464
-
-from server import server
-
-server.launch()
